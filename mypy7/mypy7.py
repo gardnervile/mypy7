92528d7997eef8334cee8222561372f0845561cf
gardnervile <lordy1jo@gmail.com>
2024-11-04 20:11:14 +0500

    исправил ошибки в оформлении

--- mypy7/mypy7.py
+++ mypy7/mypy7.py
@@ -4,18 +4,7 @@ from pytimeparse import parse
 import ptbot
 
 
-load_dotenv()
-TG_TOKEN = os.getenv('TG_TOKEN')
-TG_CHAT_ID = os.getenv('TG_CHAT_ID')
-
-PROGRESS_BAR_LENGTH = 30  
-FILL_CHAR = '█'
-EMPTY_CHAR = '░'
-
-bot = ptbot.Bot(TG_TOKEN)
-
-
-def render_progressbar(total, iteration, prefix='', suffix='', length=PROGRESS_BAR_LENGTH, fill=FILL_CHAR, zfill=EMPTY_CHAR):
+def render_progressbar(total, iteration, prefix='', suffix='', length=30, fill='█', zfill='░'):
     iteration = min(total, iteration)
     percent = "{0:.1f}".format(100 * (iteration / float(total)))
     filled_length = int(length * iteration // total)
@@ -23,31 +12,33 @@ def render_progressbar(total, iteration, prefix='', suffix='', length=PROGRESS_B
     return f'{prefix} |{pbar}| {percent}% {suffix}'
 
 
-def wait(chat_id, delay):
+def wait(bot, chat_id, delay):
     message_id = bot.send_message(chat_id, f"Осталось {delay} секунд")
-    bot.create_countdown(delay, notify_progress, chat_id=chat_id, message_id=message_id, total=delay)
-    bot.create_timer(delay, send_timeout_message, chat_id=chat_id)
+    bot.create_countdown(delay, notify_progress, bot=bot, chat_id=chat_id, message_id=message_id, total=delay)
+    bot.create_timer(delay, send_timeout_message, bot=bot, chat_id=chat_id)
 
 
-def notify_progress(secs_left, chat_id, message_id, total):
+def notify_progress(secs_left, bot, chat_id, message_id, total):
     progress_bar = render_progressbar(total, total - secs_left, prefix='Прогресс:', suffix='завершено')
     bot.update_message(chat_id, message_id, f"Осталось {secs_left} секунд\n{progress_bar}")
 
 
-def on_message(chat_id, message):
+def on_message(bot, chat_id, message):
     delay = parse(message)
     if delay is not None:
-        wait(chat_id, delay)
-    else:
-        bot.send_message(chat_id, "Не удалось распознать время. Пожалуйста, введите корректное значение.")
+        wait(bot, chat_id, delay)
 
 
-def send_timeout_message(chat_id):
+def send_timeout_message(bot, chat_id):
     bot.send_message(chat_id, "Время вышло!")
 
 
 def main():
-    bot.reply_on_message(on_message)
+    load_dotenv()
+    TG_TOKEN = os.getenv('TG_TOKEN')
+    TG_CHAT_ID = os.getenv('TG_CHAT_ID')
+    bot = ptbot.Bot(TG_TOKEN)
+    bot.reply_on_message(lambda chat_id, message: on_message(bot, chat_id, message))
     bot.run_bot()
 
 
